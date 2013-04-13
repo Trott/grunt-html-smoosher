417aa2999a48565bf51746b7fefa1d6a162ca3cf
Thanos Lefteris <alefteris@gmail.com>
2013-04-14 01:01:07 +0300

    Reimplement task using cheerio instead of jsdom

--- tasks/html_smoosher.js
+++ tasks/html_smoosher.js
@@ -6,95 +6,42 @@
  * Licensed under the MIT license.
  */
 
-'use strict';
-
 module.exports = function(grunt) {
 
+  'use strict';
+
+  var cheerio = require('cheerio');
+  var path = require('path');
+  var url = require('url');
+
   grunt.registerMultiTask('smoosher', 'Turn your distribution into something pastable.', function() {
-    var done = this.async();
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
+
+    this.files.forEach(function(filePair) {
+      // Check that the source file exists
+      if(filePair.src.length === 0) { return; }
+
+      var $ = cheerio.load(grunt.file.read(filePair.src));
+
+      $('link[rel="stylesheet"]').each(function () {
+        var style = $(this).attr('href');
+        if(!style) { return; }
+        if(style.match(/^\/\//)) { return; }
+        if(url.parse(style).protocol) { return; }
+        $(this).replaceWith('<style>' + grunt.file.read(path.join(path.dirname(filePair.src), style)) + '</style>');
+      });
+
+      $('script').each(function () {
+        var script = $(this).attr('src');
+        if(!script) { return; }
+        if(script.match(/^\/\//)) { return; }
+        if(url.parse(script).protocol) { return; }
+        $(this).replaceWith('<script>' + grunt.file.read(path.join(path.dirname(filePair.src), script)) + '</script>');
+      });
+
+      grunt.file.write(filePair.dest, $.html());
+      grunt.log.writeln('File "' + filePair.dest + '" created.');
     });
 
-    // Iterate over all specified file groups.
-    grunt.util.async.forEachSeries(this.files, function(f, nextFileObj) {
-      var destFile = f.dest;
-
-      var files = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      });
-
-      if (files.length === 0) {
-        // No src files, goto next target. Warn would have been issued above.
-        nextFileObj();
-      }
-
-      var compiled = [];
-      grunt.util.async.concatSeries(files, function(file, next) {
-        smoosh(file, options, function(snippet, err) {
-          if (!err) {
-            compiled.push(snippet);
-            next(null);
-          } else {
-            nextFileObj(false);
-          }
-        });
-      }, function() {
-        if (compiled.length < 1) {
-          grunt.log.warn('Destination not written because compiled files were empty.');
-        } else {
-          grunt.file.write(destFile, compiled.join(grunt.util.normalizelf(grunt.util.linefeed)));
-          grunt.log.writeln('File ' + destFile.cyan + ' created.');
-        }
-        nextFileObj();
-      });
-    }, done);
   });
-
-  var smoosh = function(srcFile, options, callback) {
-    try {
-      var srcCode = grunt.file.read(srcFile);
-      var jsdom = require('jsdom');
-      var jQueryLink = "http://code.jquery.com/jquery.js";
-      grunt.util.async.series(
-        {
-          remakehtml: function(cb) {
-            jsdom.env(
-              srcCode,
-              [jQueryLink],
-              cb
-            );
-          }
-        },
-        function(errors, window) {
-          var html = window.remakehtml;
-          var $ = window.remakehtml.$;
-          $('script').each(function() {
-            var jsFile = $(this).attr('src');
-            if (jsFile !== jQueryLink) {
-              var js = grunt.file.read(srcFile.replace(/\/[^\/]+?$/, '/') + jsFile);
-              $(this).replaceWith('<script type="text/javascript">' + js + '</script>');
-            }
-          });
-          $('link').each(function() {
-            var cssFile = $(this).attr('href');
-            var css = grunt.file.read(srcFile.replace(/\/[^\/]+?$/, '/') + cssFile);
-            $(this).replaceWith('<style type="text/css">' + css + '</style>');
-          });
-          //ugh. linebreaks. this is stupid
-          callback($(html.document._documentElement).html() + '\n', null);
-        }
-      );
-    } catch (e) {
-      grunt.log.error(e);
-      callback(e, true);
-    }
-  };
 
 };
