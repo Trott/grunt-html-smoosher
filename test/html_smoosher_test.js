ecddf82e5282f5fda7045b28e34b9f686ae047b0
Rich Trott <rtrott@gmail.com>
2021-08-12 11:21:08 -0700

    chore: update dependencies (#44)

--- test/html_smoosher_test.js
+++ test/html_smoosher_test.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var grunt = require('grunt');
-var assert = require('assert');
+const grunt = require('grunt');
+const assert = require('assert');
 
 // default_options:
-var actual = grunt.file.read('tmp/default_options');
-var expected = grunt.file.read('test/expected/default_options');
+const actual = grunt.file.read('tmp/default_options');
+const expected = grunt.file.read('test/expected/default_options');
 assert.strictEqual(actual.replace(/\s/g, ''), expected.replace(/\s/g, ''), 'should describe what the default behavior is.');
